c003a85f7d4c9be531cbb9ee8217a35351b6fd77
Ilya Sergey <ilya.sergey@yale-nus.edu.sg>
2019-03-04 16:50:09 +0800

    update

--- lib/week_08_Tests.ml
+++ lib/week_08_Tests.ml
@@ -9,4 +9,11 @@ let%test "ListBasedHashTable insert" =
   let open SimpleHTTester in
   let a = generate_key_value_array 1000 in
   let ht = mk_test_table_from_array_length a 50 in
-  test_table_get ht a;
+  test_table_get ht a
+
+let%test "ListBasedHashTable insert" = 
+  let open ResizableHTTester in
+  let a = generate_key_value_array 1000 in
+  let ht = mk_test_table_from_array_length a 50 in
+  test_table_get ht a
+
